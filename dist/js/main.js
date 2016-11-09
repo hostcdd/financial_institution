2c29cd0eb991ffe25fe14c1bd0f6bd8573f91111
caodongdong <caodongdong@gomeholdings.com>
2016-11-09 17:03:46 +0800

    dist

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,6 @@
 /**
  * Created by Administrator on 2016/6/21.
  */
-define(function(){require.config({waitSeconds:0,baseUrl:"js",paths:{jquery:"../../js/vendor/jquery.min",bootstrap:"../../js/vendor/bootstrap.min",mustache:"../../js/vendor/mustache.min",public:"../../js/common/public",entAccount:"../../js/module/entAccount",validate:"../../js/common/validate",api:"../../js/common/api",hash:"../../js/vendor/hash"},shim:{bootstrap:{deps:["jquery"],exports:"bootstrap"},hash:{deps:["jquery"],exports:"hash"}}}),require(["jquery"],function($){$("#navBar a").click(function(){var e=$(this).attr("data-url");$("iframe").attr("src",e)});
+define(function(){require.config({waitSeconds:0,baseUrl:"js",paths:{jquery:"../../js/vendor/jquery.min",bootstrap:"../../js/vendor/bootstrap.min",mustache:"../../js/vendor/mustache.min",public:"../../js/common/public",entAccount:"../../js/module/entAccount",validate:"../../js/common/validate",api:"../../js/common/api",hash:"../../js/vendor/hash"},shim:{bootstrap:{deps:["jquery"],exports:"bootstrap"},hash:{deps:["jquery"],exports:"hash"}}}),require(["jquery"],function($){$("#navBar a").click(function(){var url=$(this).attr("data-url");$("iframe").attr("src",url)});
 // 调整iframe的高度
-var e=$("#headerTitle1").height(),t=$(window).height(),a=$("#content-page"),o=a.width();a.height(t-e-50+"px").width(o-4+"px")})});
+var headerHeight=$("#headerTitle1").height(),windowHeight=$(window).height(),$content=$("#content-page"),contentWidth=$content.width();$content.height(windowHeight-headerHeight-50+"px").width(contentWidth-4+"px")})});
