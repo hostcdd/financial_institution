63c833be9403b4cc5583cccb646fb4521397fc63
chenshuai <chenshuai@gomeholdings.com>
2016-11-09 19:50:02 +0800

    ----

--- dist/js/securityCenter.js
+++ dist/js/securityCenter.js
@@ -1,1 +1,3 @@
-$(function(){});
+$(function(){
+// 显示邮箱修改弹窗
+function showEmailInfo(){$("#myModal4Notify").modal("show")}$("#modEmail").click(function(){$("#myModal4email").modal("hide"),showEmailInfo()})});
--- src/js/securityCenter.js
+++ src/js/securityCenter.js
@@ -1,3 +1,10 @@
 $(function(){
-    
+    // 显示邮箱修改弹窗
+    function showEmailInfo(){
+        $('#myModal4Notify').modal('show');
+    }
+    $("#modEmail").click(function(){
+        $("#myModal4email").modal('hide');
+        showEmailInfo();
+    })
 })
