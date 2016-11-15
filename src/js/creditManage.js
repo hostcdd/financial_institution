120fa2f1576fbfd5edffb06758a3b7551ecc4d80
chenshuai <chenshuai@gomeholdings.com>
2016-11-15 20:01:09 +0800

    dddd

--- src/js/creditManage.js
+++ src/js/creditManage.js
@@ -12,12 +12,19 @@ $(function(){
       forceParse: 0
   });
 
+  // 选中或取消选中供应商时添加或删除效果
   $(".supplier a").click(function(){
     $(this).toggleClass("supplier-sel");
   });
 
+  // 展开或显示供应商
   $("#moreSupplier").click(function(){
   	$(".supplier").toggleClass("supplier-height");
   	$(this).text()=="更多"?$(this).text("收起"):$(this).text("更多");
+  });
+
+  // 跳转到详情
+  $(".go-detail").click(function(){
+  	top.changeFrameUrl(top.URLOBJ.creditDetail);
   });
 })
