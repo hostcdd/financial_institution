aa7aef989409ba5d0229b719cebf5fda88081a4a
dingtt <dingtingting@gomeholdings.com>
2016-12-01 15:33:40 +0800

    test

--- dist/js/creditManage.js
+++ dist/js/creditManage.js
@@ -1,1 +1,9 @@
-$(function(){alert("this is test2")});
+$(function(){
+// 初始化日期控件
+$(".form_datetime").datetimepicker({language:"zh-CN",format:"yyyy-mm-dd",weekStart:1,todayBtn:1,autoclose:1,todayHighlight:1,startView:2,minView:2,forceParse:0}),
+// 选中或取消选中供应商时添加或删除效果
+$(".supplier a").click(function(){$(this).toggleClass("supplier-sel")}),
+// 展开或显示供应商
+$("#moreSupplier").click(function(){$(".supplier").toggleClass("supplier-height"),"更多"==$(this).text()?$(this).text("收起"):$(this).text("更多")}),
+// 跳转到详情
+$(".go-detail").click(function(){top.changeFrameUrl(top.URLOBJ.creditDetail)})});
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,7 @@
 $(function(){$("#navBar a").click(function(){var url=$(this).attr("data-url");$("iframe").attr("src",url)});
 // 调整iframe的高度
-var headerHeight=$("#headerTitle1").height(),windowHeight=$(window).height(),$content=$("#content-page"),contentWidth=$content.width();$content.height(windowHeight-headerHeight-50+"px").width(contentWidth-4+"px")});
+var headerHeight=$("#headerTitle1").height(),windowHeight=$(window).height(),$content=$("#content-page"),contentWidth=$content.width();$content.height(windowHeight-headerHeight-50+"px").width(contentWidth-4+"px"),
+// 将url填入页面
+function(){for(key in URLOBJ){var $menu=$("#"+key);$menu.length>0&&$menu.attr("data-url",URLOBJ[key])}}(),
+// 点击显示子菜单
+$("#navBar .parentMenu").click(function(){var $targetUl=$(this).next();$targetUl.hasClass("hidden")?($("#navBar .parentMenu+ul").addClass("hidden"),$targetUl.removeClass("hidden")):$targetUl.addClass("hidden")})});
